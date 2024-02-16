7c5580d08758a1db536a667143cd41134d12fb57
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-02-16 13:51:29 +0100

    chore: Add Jest dependencies and configuration for running tests

---
+++ config/jest.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+	preset: 'ts-jest',
+	rootDir: '..',
+	testEnvironment: 'node',
+}
