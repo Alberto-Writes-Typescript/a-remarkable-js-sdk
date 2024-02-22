433e299f5aef4fe147ce5d85f5262e2de43ef506
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-02-22 11:01:49 +0100

    First successful integration of Polly.js in test
    Jest now runs test in web-browser environment. Polly.js is capable of recording test HTTP requests performed through `fetch`, creating an `.har` file with all the response information.
    
    Any other test within the same namespace performing the same HTTP requests receives the same response as result.

--- test/net/FetchClient.test.ts
+++ test/net/FetchClient.test.ts
@@ -1,26 +1,45 @@
 // @ts-nocheck
-
+import 'whatwg-fetch'
+import { Polly } from '@pollyjs/core'
+import * as FSPersister from '@pollyjs/persister-fs'
+import * as FetchAdapter from '@pollyjs/adapter-fetch'
 import { FetchClient } from '../../src/net/FetchClient'
 
+Polly.register(FetchAdapter)
+Polly.register(FSPersister)
+
 describe('FetchClient', () => {
-  const SAMPLE_HOST = 'https://jsonplaceholder.typicode.com'
-  const SAMPLE_PATH = '/todos/1'
+  const SAMPLE_HOST: string = 'https://jsonplaceholder.typicode.com'
+  const SAMPLE_PATH: string = '/todos/1'
 
-  let fetchClient: FetchClient
-
-  beforeAll(() => {
-    fetchClient = new FetchClient()
-  })
+  let fetchClient = new FetchClient()
+  let polly: Polly
 
   describe('#makeRequest', () => {
     it('returns Response object', async () => {
+      let polly = new Polly('#makeRequest', {
+        adapters: ['fetch'],
+        persister: 'fs',
+        persisterOptions: { fs: { recordingsDir: '__recordings__' } }
+      })
+
       const response = await fetchClient.makeRequest(SAMPLE_HOST, SAMPLE_PATH, 'GET')
       expect(response).toBeInstanceOf(Response)
+
+      await polly.stop()
     })
 
     it('performs request to given URL', async () => {
+      let polly = new Polly('#makeRequest', {
+        adapters: ['fetch'],
+        persister: 'fs',
+        persisterOptions: { fs: { recordingsDir: '__recordings__' } }
+      })
+
       const response = await fetchClient.makeRequest(SAMPLE_HOST, SAMPLE_PATH, 'GET')
       expect(response.url).toBe(SAMPLE_HOST + SAMPLE_PATH)
+
+      await polly.stop()
     })
 
     it('performs request with given headers', async () => {
