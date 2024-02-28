91ae89f0f2dffa875fe01bc3966f68de66cd50bd
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-02-28 09:24:08 +0100

    Replace hard-coded test variables by variables placed under `.env.test`.
    This is the first step to remove any sensible information from tests (like tokens, UUIDs and so on).

--- test/authentication/Device.test.ts
+++ test/authentication/Device.test.ts
@@ -1,14 +1,9 @@
 /**
  * @jest-environment node
  */
-
 import Device from '../../src/authentication/Device'
 import { setupHttpRecording } from '../helpers/pollyHelpers'
 import DeviceToken from '../../src/authentication/DeviceToken'
-
-// const SAMPLE_ONE_TIME_CODE: string = 'cfdaemci'
-const SAMPLE_UUID: string = '02ce7950-0b1e-4039-95a7-e098e10c33fa'
-const SAMPLE_PAIR_TOKEN: DeviceToken = new DeviceToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoMC11c2VyaWQiOiJhdXRoMHw1ZWZjMjg3ZmM0ODgwMTAwMTM0NGY5MjMiLCJkZXZpY2UtZGVzYyI6ImJyb3dzZXItY2hyb21lIiwiZGV2aWNlLWlkIjoiMDJjZTc5NTAtMGIxZS00MDM5LTk1YTctZTA5OGUxMGMzM2ZhIiwiaWF0IjoxNzA5MDE2NTk3LCJpc3MiOiJyTSBXZWJBcHAiLCJqdGkiOiJjazB0S2FFdFM4MD0iLCJuYmYiOjE3MDkwMTY1OTcsInN1YiI6InJNIERldmljZSBUb2tlbiJ9.vO5iXB-Xy1cbpn8HeZmt5NYyI3wytnPxYp2-2WggFhU')
 
 describe('Device', () => {
   // Enables Polly.js to record and replay HTTP requests for each test
@@ -35,11 +30,15 @@ describe('Device', () => {
     it(
       'given valid pair token, updates Device with new session token',
       async () => {
-        const device = new Device(SAMPLE_UUID, 'browser-chrome', SAMPLE_PAIR_TOKEN)
+        const device = new Device(
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          process.env.SAMPLE_UUID!, 'browser-chrome', new DeviceToken(process.env.SAMPLE_PAIR_TOKEN!)
+        )
 
         await device.connect()
 
-        expect(device.sessionToken.deviceId).toBe(SAMPLE_UUID)
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        expect(device.sessionToken.deviceId).toBe(process.env.SAMPLE_UUID!)
         expect(device.sessionToken.deviceDescription).toBe('browser-chrome')
       },
       30000
--- test/authentication/DeviceToken.test.ts
+++ test/authentication/DeviceToken.test.ts
@@ -1,13 +1,14 @@
 import DeviceToken from '../../src/authentication/DeviceToken'
 
-const SAMPLE_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoMC11c2VyaWQiOiJhdXRoMHw1ZWZjMjg3ZmM0ODgwMTAwMTM0NGY5MjMiLCJkZXZpY2UtZGVzYyI6ImJyb3dzZXItY2hyb21lIiwiZGV2aWNlLWlkIjoiZmQwNDNkNGItYWUzNy00NTJlLThkMjAtNmE1MGVkY2M0YmZjIiwiaWF0IjoxNzA4OTM0MDAyLCJpc3MiOiJyTSBXZWJBcHAiLCJqdGkiOiJjazB0dXhrbkhQOD0iLCJuYmYiOjE3MDg5MzQwMDIsInN1YiI6InJNIERldmljZSBUb2tlbiJ9.Tbb557AZHRCO4BFmTKXm1jDNRV9PdjMWTwZPo9eDYzw'
-
 describe('DeviceToken', () => {
   it('given a valid token, returns token with device information', () => {
-    const token = new DeviceToken(SAMPLE_TOKEN)
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const token = new DeviceToken(process.env.SAMPLE_PAIR_TOKEN!)
 
-    expect(token.token).toBe(SAMPLE_TOKEN)
-    expect(token.deviceId).toBe('fd043d4b-ae37-452e-8d20-6a50edcc4bfc')
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    expect(token.token).toBe(process.env.SAMPLE_PAIR_TOKEN!)
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    expect(token.deviceId).toBe(process.env.SAMPLE_UUID!)
     expect(token.deviceDescription).toBe('browser-chrome')
     expect(token.issuer).toBe('rM WebApp')
     expect(token.subject).toBe('rM Device Token')
