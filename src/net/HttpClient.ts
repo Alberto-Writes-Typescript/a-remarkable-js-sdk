a32bffd263abd89960b000ed629134284d10fd28
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-03-01 13:57:58 +0100

    Add new HttpClientContext interface and define HttpClient instance with context.
    Now `HttpClient` classes can be instantiated with `host` and `headers`. This way, all request performed through the instance will reuse the same parameters in all request.
    
    Instance methods still admit contexts to overwrite the default instance parameters if necessary. This approach will simplify the way we use contexts in our different classes and modules.

--- src/net/HttpClient.ts
+++ src/net/HttpClient.ts
@@ -1,4 +1,29 @@
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+import HttpClientContext from './HttpClientContext'
+
+/**
+ * HTTP Client abstract class
+ *
+ * Defines interface for HTTP clients.
+ *
+ * Used by the different clients of the current library, to provide
+ * implementations of the HTTP interface with different libraries
+ * (https, fetch, etc).
+ *
+ * HTTP requests through client can be performed in two different ways:
+ *
+ * - If you are performing a specific HTTP requests with a host and headers
+ *   which are not used in other requests, you can use the static methods
+ *   of the client (get, post, patch, put, delete).
+ *
+ * - If you are performing multiple requests to the same endpoint with
+ *   the same headers, you can create an instance and use the instance
+ *   methods (get, post, patch, put, delete). This way, you can perform
+ *   multiple requests to the same host/with the same headers without
+ *   specifying them every time.
+ *
+ * @abstract
+ * @class HttpClient - HTTP Client abstract class
+ */
 export default abstract class HttpClient {
   public static async get (
     host: string,
@@ -41,5 +66,54 @@ export default abstract class HttpClient {
     headers: Record<string, string> = {}
   ): Promise<Response> {
     throw new Error('HTTP Client does not implement delete static method')
+  }
+
+  readonly context: HttpClientContext
+
+  constructor (host: string, headers: Record<string, string> = {}) {
+    this.context = { host, headers }
+  }
+
+  public async get (
+    path: string,
+    context: HttpClientContext = this.context
+  ): Promise<Response> {
+    return (this.constructor as any)
+      .get(context.host || this.context.host, path, context.headers || this.context.headers)
+  }
+
+  public async post (
+    path: string,
+    body: Record<string, string> = {},
+    context: HttpClientContext = this.context
+  ): Promise<Response> {
+    return (this.constructor as any)
+      .post(context.host || this.context.host, path, context.headers || this.context.headers, body)
+  }
+
+  public async patch (
+    path: string,
+    body: Record<string, string> = {},
+    context: HttpClientContext = this.context
+  ): Promise<Response> {
+    return (this.constructor as any)
+      .patch(context.host || this.context.host, path, context.headers || this.context.headers, body)
+  }
+
+  public async put (
+    path: string,
+    body: Record<string, string> = {},
+    context: HttpClientContext = this.context
+  ): Promise<Response> {
+    return (this.constructor as any)
+      .put(context.host || this.context.host, path, context.headers || this.context.headers, body)
+  }
+
+  public async delete (
+    path: string,
+    context: HttpClientContext = this.context
+  ): Promise<Response> {
+    return (this.constructor as any)
+      .delete(context.host || this.context.host, path, context.headers || this.context.headers)
   }
 }
