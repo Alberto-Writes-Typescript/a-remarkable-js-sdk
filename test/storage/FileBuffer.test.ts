f391f63e718e2937ebdacda0f987d14bd023b1c7
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-03-11 10:10:24 +0100

    Add test coverage for PDF upload

--- test/storage/FileBuffer.test.ts
+++ test/storage/FileBuffer.test.ts
@@ -1,11 +1,11 @@
 import Device from '../../src/authentication/Device'
-import ServiceManager from '../../src/ServiceManager'
 import DeviceToken from '../../src/authentication/DeviceToken'
-import FileBuffer, { FileNoUploadedError } from '../../src/storage/FileBuffer'
+import FileBuffer from '../../src/storage/FileBuffer'
+import ServiceManager from '../../src/ServiceManager'
 import { setupHttpRecording } from '../helpers/pollyHelpers'
 
 describe('FileBuffer', () => {
-  let buffer: FileBuffer = null
+  let serviceManager: ServiceManager = null
 
   setupHttpRecording()
 
@@ -18,34 +18,24 @@ describe('FileBuffer', () => {
 
     await device.connect()
 
-    const serviceManager = new ServiceManager(device)
-
-    buffer = await FileBuffer.fromLocalFile('./test/fixtures/documents/sample.epub', serviceManager)
+    serviceManager = new ServiceManager(device)
   })
 
   describe('.upload', () => {
     it('given an e-pub file buffer, uploads file', async () => {
+      const buffer = await FileBuffer.fromLocalFile('./test/fixtures/documents/sample.epub', serviceManager)
+
       await buffer.upload()
 
       expect(buffer.uploaded).toBe(true)
     }, 10000000)
-  })
 
-  describe('.remarkableDocumentId', () => {
-    it('if FileBuffer is uploaded, returns document ID', async () => {
+    it('given a pdf file buffer, uploads file', async () => {
+      const buffer = await FileBuffer.fromLocalFile('./test/fixtures/documents/sample.pdf', serviceManager)
+
+      await buffer.upload()
+
+      expect(buffer.uploaded).toBe(true)
     }, 10000000)
-
-    it('if FileBuffer is not uploaded, throws exception', async () => {
-      expect(() => buffer.remarkableDocumentId).toThrow(FileNoUploadedError)
-    })
-  })
-
-  describe('.remarkableDocumentHash', () => {
-    it('if FileBuffer is uploaded, returns document Hash', async () => {
-    }, 10000000)
-
-    it('if FileBuffer is not uploaded, throws exception', async () => {
-      expect(() => buffer.remarkableDocumentHash).toThrow(FileNoUploadedError)
-    })
   })
 })
