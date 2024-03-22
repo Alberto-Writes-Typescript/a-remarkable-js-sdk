b1835c8ad55aee82f965ae94e2d790e5740f9fa6
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-03-22 14:28:22 +0100

    feature: File System
    Implemented logic to recreate the reMarkable cloud file tree.
    
    The FileSystem class retrieves all `Documents` and `Folders` metadata in a reMarkable account, returning a collection of objects interconected in away they resemble the actual file tree.

---
+++ src/internal/FileSystem.ts
@@ -0,0 +1,129 @@
+import Document from './Document'
+import Folder from './Folder'
+import type HttpClient from '../net/HttpClient'
+import HttpClientContext from '../net/HttpClientContext'
+import type ServiceManager from '../ServiceManager'
+
+export interface DocumentPayload {
+  hash: string
+  id: string
+  type: 'DocumentType'
+  fileType: 'pdf' | 'epub' | 'notebook'
+  visibleName: string
+  lastModified?: string
+  lastOpened?: string
+  parent?: string
+  pinned: boolean
+}
+
+export interface FolderPayload {
+  id: string
+  hash: string
+  type: 'CollectionType'
+  visibleName: string
+  lastModified?: string
+  parent?: string
+  pinned: boolean
+}
+
+export class FileSystemParser {
+  documentPayloads: DocumentPayload[]
+  folderPayloads: FolderPayload[]
+  documents: Document[]
+  folders: Folder[]
+
+  constructor (rawFileSystemPayload: Array<DocumentPayload | FolderPayload>) {
+    this.documentPayloads = rawFileSystemPayload.filter((payload) => payload.type === 'DocumentType') as DocumentPayload[]
+    this.folderPayloads = rawFileSystemPayload.filter((payload) => payload.type === 'CollectionType') as FolderPayload[]
+
+    this.parse()
+  }
+
+  private parse (): void {
+    this.parseFolders()
+    this.parseDocuments()
+    this.buildFileTreeHierarchy()
+  }
+
+  private parseFolders (): void {
+    this.folders = this.folderPayloads.map((folderPayload) => {
+      return new Folder(
+        folderPayload.id,
+        folderPayload.hash,
+        folderPayload.visibleName,
+        (folderPayload.lastModified != null) ? new Date(folderPayload.lastModified) : null,
+        undefined,
+        [],
+        []
+      )
+    })
+  }
+
+  private parseDocuments (): void {
+    this.documents = this.documentPayloads.map((documentPayload) => {
+      return new Document(
+        documentPayload.id,
+        documentPayload.hash,
+        documentPayload.visibleName,
+        documentPayload.fileType,
+        (documentPayload.lastModified != null) ? new Date(documentPayload.lastModified) : null,
+        (documentPayload.lastOpened != null) ? new Date(documentPayload.lastOpened) : null,
+        null
+      )
+    })
+  }
+
+  private buildFileTreeHierarchy (): void {
+    this.folderPayloads.forEach((folderPayload) => {
+      const folder = this.folders.find((f) => f.id === folderPayload.id)
+
+      if (folderPayload.parent != null && folder.parentFolder == null) {
+        folder.parentFolder = this.folders.find((f) => f.id === folderPayload.parent)
+      }
+
+      const folderDocumentPayloads = this.documentPayloads.filter((documentPayload) => documentPayload.parent === folder.id)
+      const folderDocuments = folderDocumentPayloads.map((documentPayload) => this.documents.find((d) => d.id === documentPayload.id))
+
+      folderDocuments.forEach((document) => {
+        document.folder = folder
+        folder.documents.push(document)
+      })
+
+      const folderFolderPayloads = this.folderPayloads.filter((folderPayload) => folderPayload.parent === folder.id)
+      const folderFolders = folderFolderPayloads.map((folderPayload) => this.folders.find((f) => f.id === folderPayload.id))
+
+      folderFolders.forEach((subFolder) => {
+        subFolder.parentFolder = folder
+        folder.folders.push(subFolder)
+      })
+    })
+  }
+}
+
+export default class FileSystem {
+  static async initialize (serviceManager: ServiceManager): Promise<FileSystem> {
+    const httpClient: HttpClient = await serviceManager.internalCloudHttpClient()
+
+    const response = await httpClient.get(
+      '/doc/v2/files',
+      new HttpClientContext(null, { 'rm-source': 'RoR-Browser' })
+    )
+
+    if (response.status !== 200) {
+      throw new Error(`Error during file system initialization: ${await response.text()}`)
+    }
+
+    const fileSystemPayload = JSON.parse(await response.text()) as Array<DocumentPayload | FolderPayload>
+    const fileSystemParser = new FileSystemParser(fileSystemPayload)
+
+    return new FileSystem(fileSystemParser.documents, fileSystemParser.folders)
+  }
+
+  documents: Document[] = []
+  folders: Folder[] = []
+
+  constructor (documents: Document[], folders: Folder[]) {
+    this.documents = documents
+    this.folders = folders
+  }
+}
---
+++ src/internal/Folder.ts
@@ -0,0 +1,21 @@
+import type Document from './Document'
+
+export default class Folder {
+  id: string
+  hash: string
+  name: string
+  folders?: Folder[] = []
+  documents?: Document[] = []
+  parentFolder?: Folder
+  lastModified?: Date
+
+  constructor (id: string, hash: string, name: string, lastModified: Date, parentFolder?: Folder, folders?: Folder[], documents?: Document[]) {
+    this.id = id
+    this.hash = hash
+    this.name = name
+    this.lastModified = lastModified
+    this.parentFolder = parentFolder
+    this.folders = folders
+    this.documents = documents
+  }
+}
