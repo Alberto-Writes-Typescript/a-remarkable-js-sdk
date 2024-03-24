a8c413bd095b006fe9a7917007d7f117e1034be6
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-03-24 17:43:36 +0100

    Update `index.ts` to export all classes in our library.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-export function sum (num1: number, num2: number): number {
-  return num1 + num2
+import Device from './authentication/Device'
+import FileBufferType from './internal/FileBufferType'
+import DeviceToken from './authentication/DeviceToken'
+import FileBuffer from './internal/FileBuffer'
+import HashUrl from './internal/HashUrl'
+import HttpClient from './net/HttpClient'
+import HttpClientContext from './net/HttpClientContext'
+import HttpClientRequest from './net/HttpClientRequest'
+import HttpClientRequestBody from './net/HttpClientRequestBody'
+import NodeClient from './net/NodeClient'
+import ServiceManager from './ServiceManager'
+
+export {
+  Device, DeviceToken,
+  FileBuffer, FileBufferType, HashUrl,
+  HttpClient, HttpClientContext, HttpClientRequest, HttpClientRequestBody, NodeClient,
+  ServiceManager
 }
