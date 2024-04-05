042eb3c70ea1860d04cfe380325e81d709e64e7c
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-05 11:10:15 +0200

    feat: Add feature to fetch documents and folders from file system by ID

--- test/internal/FileSystem.test.ts
+++ test/internal/FileSystem.test.ts
@@ -3,66 +3,66 @@ import ServiceManager from '../../src/serviceDiscovery/ServiceManager'
 import { setupHttpRecording } from '../helpers/pollyHelpers'
 import { Session } from '../../src'
 
-describe('FileSystemParser', () => {
-  /**
-   * - file-without-parent
-   * - folder-1
-   *   |- folder-1-file-1
-   *   |- folder-1-subfolder-1
-   *     |- subfolder-1-file-1
-   */
-  const sampleFileSystemPayload: Array<DocumentPayload | FolderPayload> = [
-    {
-      id: 'file-without-parent',
-      hash: 'file-without-parent',
-      type: 'DocumentType',
-      visibleName: 'file-without-parent',
-      fileType: 'pdf',
-      lastModified: '2021-01-01T00:00:00.000Z',
-      lastOpened: '2021-01-01T00:00:00.000Z',
-      pinned: true
-    },
-    {
-      id: 'folder-1',
-      hash: 'folder-1',
-      type: 'CollectionType',
-      visibleName: 'folder-1',
-      lastModified: '2021-01-01T00:00:00.000Z',
-      pinned: true
-    },
-    {
-      id: 'folder-1-file-1',
-      hash: 'folder-1-file-1',
-      type: 'DocumentType',
-      visibleName: 'folder-1-file-1',
-      fileType: 'pdf',
-      lastModified: '2021-01-01T00:00:00.000Z',
-      lastOpened: '2021-01-01T00:00:00.000Z',
-      parent: 'folder-1',
-      pinned: true
-    },
-    {
-      id: 'folder-1-subfolder-1',
-      hash: 'folder-1-subfolder-1',
-      type: 'CollectionType',
-      visibleName: 'folder-1-subfolder-1',
-      lastModified: '2021-01-01T00:00:00.000Z',
-      parent: 'folder-1',
-      pinned: true
-    },
-    {
-      id: 'subfolder-1-file-1',
-      hash: 'subfolder-1-file-1',
-      type: 'DocumentType',
-      visibleName: 'subfolder-1-file-1',
-      fileType: 'pdf',
-      lastModified: '2021-01-01T00:00:00.000Z',
-      lastOpened: '2021-01-01T00:00:00.000Z',
-      parent: 'folder-1-subfolder-1',
-      pinned: true
-    }
-  ]
+/**
+ * - file-without-parent
+ * - folder-1
+ *   |- folder-1-file-1
+ *   |- folder-1-subfolder-1
+ *     |- subfolder-1-file-1
+ */
+const sampleFileSystemPayload: Array<DocumentPayload | FolderPayload> = [
+  {
+    id: 'file-without-parent',
+    hash: 'file-without-parent',
+    type: 'DocumentType',
+    visibleName: 'file-without-parent',
+    fileType: 'pdf',
+    lastModified: '2021-01-01T00:00:00.000Z',
+    lastOpened: '2021-01-01T00:00:00.000Z',
+    pinned: true
+  },
+  {
+    id: 'folder-1',
+    hash: 'folder-1',
+    type: 'CollectionType',
+    visibleName: 'folder-1',
+    lastModified: '2021-01-01T00:00:00.000Z',
+    pinned: true
+  },
+  {
+    id: 'folder-1-file-1',
+    hash: 'folder-1-file-1',
+    type: 'DocumentType',
+    visibleName: 'folder-1-file-1',
+    fileType: 'pdf',
+    lastModified: '2021-01-01T00:00:00.000Z',
+    lastOpened: '2021-01-01T00:00:00.000Z',
+    parent: 'folder-1',
+    pinned: true
+  },
+  {
+    id: 'folder-1-subfolder-1',
+    hash: 'folder-1-subfolder-1',
+    type: 'CollectionType',
+    visibleName: 'folder-1-subfolder-1',
+    lastModified: '2021-01-01T00:00:00.000Z',
+    parent: 'folder-1',
+    pinned: true
+  },
+  {
+    id: 'subfolder-1-file-1',
+    hash: 'subfolder-1-file-1',
+    type: 'DocumentType',
+    visibleName: 'subfolder-1-file-1',
+    fileType: 'pdf',
+    lastModified: '2021-01-01T00:00:00.000Z',
+    lastOpened: '2021-01-01T00:00:00.000Z',
+    parent: 'folder-1-subfolder-1',
+    pinned: true
+  }
+]
 
+describe('FileSystemParser', () => {
   it('parses folders', () => {
     const fileSystemParser = new FileSystemParser(sampleFileSystemPayload)
 
@@ -146,6 +146,46 @@ describe('FileSystem', () => {
       const fileSystem = await FileSystem.initialize(serviceManager)
 
       expect(fileSystem.documents.length).toBe(global.unitTestParams.fileSystemDocumentsCount)
+    })
+  })
+
+  describe('document', () => {
+    let fileSystem: FileSystem = null
+
+    beforeAll(() => {
+      const fileSystemParser = new FileSystemParser(sampleFileSystemPayload)
+      fileSystem = new FileSystem(fileSystemParser.documents, fileSystemParser.folders)
+    })
+
+    it('if there is a document with given ID, returns document', async () => {
+      const document = fileSystem.document('file-without-parent')
+
+      expect(document).not.toBeNull()
+      expect(document.id).toBe('file-without-parent')
+    })
+
+    it('if there is no document with given ID, returns null', async () => {
+      expect(fileSystem.document('non-existent-document')).not.toBeDefined()
+    })
+  })
+
+  describe('folder', () => {
+    let fileSystem: FileSystem = null
+
+    beforeAll(() => {
+      const fileSystemParser = new FileSystemParser(sampleFileSystemPayload)
+      fileSystem = new FileSystem(fileSystemParser.documents, fileSystemParser.folders)
+    })
+
+    it('if there is a folder with given ID, returns folder', async () => {
+      const folder = fileSystem.folder('folder-1')
+
+      expect(folder).not.toBeNull()
+      expect(folder.id).toBe('folder-1')
+    })
+
+    it('if there is no folder with given ID, returns null', async () => {
+      expect(fileSystem.folder('non-existent-folder')).not.toBeDefined()
     })
   })
 })
