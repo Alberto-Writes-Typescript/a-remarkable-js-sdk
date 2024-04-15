deb51bc931f095e65b18230300e8f4d3c1f47b72
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-15 08:07:50 +0200

    feat: File System Snapshot.
    Added a new `internal` class to take snapshots of the reMarkable FileSystem.
    
    The idea behind is for the `FileSystem` to always perform HTTP requests to the API to fetch the most up to date version of the FileSystem elements; and the snapshot to provide the FileSystem cache.
    
    This way we do not need to worry about adding any sort of complex cache-like logic in our code. It is up to the developers how do they want to make use of the snapshots as a cache for optimizing operations.

---
+++ src/internal/FileSystemSnapshot.ts
@@ -0,0 +1,40 @@
+import type Document from './Document'
+import type Folder from './Folder'
+
+/**
+ * Represents a snapshot of the reMarkable Cloud API file system.
+ *
+ * Provides a list of all { @link Document }s and { @link Folder }s in
+ * the user's reMarkable Cloud account in a specific point in time.
+ *
+ * A snapshot is valid as long as the user does not perform any write
+ * operations on the reMarkable Cloud file system. As soon a file in
+ * the system is uploaded or modified, all the files hashes are modify,
+ * causing the snapshot to be de-synchornized with the actual reMarkable
+ * Cloud file system.
+ *
+ * You can see the snapshot as a cache of the reMarkable Cloud file system.
+ */
+export default class FileSystemSnapshot {
+  /**
+   * List of all {@link Document}s in user reMarkable Cloud account
+   */
+  readonly #documents: Document[]
+  /**
+   * List of all {@link Folder}s in user reMarkable Cloud account
+   */
+  readonly #folders: Folder[]
+
+  constructor (documents: Document[], folders: Folder[]) {
+    this.#documents = documents
+    this.#folders = folders
+  }
+
+  document (id: string): Document | undefined {
+    return this.#documents.find((document) => document.id === id)
+  }
+
+  folder (id: string): Folder | undefined {
+    return this.#folders.find((folder) => folder.id === id)
+  }
+}
